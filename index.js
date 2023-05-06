dc40343faa6530bb5619a9d1902a093c1ccc3e80
Sourin <iamsourinsarkar@gmail.com>
2023-05-06 23:01:35 +0530

    Implemeted readFile using utf8 encoding

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // One way: add toString() to data in console
 // Second way: ass UTF-8 encoding as a readFile parameter
 
-fs.readFile('./files/starter.txt', (err, data) => {
+fs.readFile('./files/starter.txt', 'utf8', (err, data) => {
     if(err) throw err;
-    console.log(data.toString());
+    console.log(data);
 });
