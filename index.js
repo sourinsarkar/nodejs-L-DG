cc80bfb211323b465c1359c97c98888f0d0a1289
Sourin <iamsourinsarkar@gmail.com>
2023-05-07 01:04:15 +0530

    Origin needs files

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
+const path = require('path'); 
 
 // One way: add toString() to data in console
 // Second way: ass UTF-8 encoding as a readFile parameter
-
-fs.readFile('./files/starter.txt', 'utf8', (err, data) => {
+// Learning redFile method of JS
+fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
     if(err) throw err;
     console.log(data);
+});
+
+console.log('Hello...');
+
+// Learning writeFile method of JS
+fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you', (err) => {
+    if(err) throw err;
+    console.log('Write complete');
+});
+
+
+process.on('uncaughtException', err => {
+    console.log(`There was an uncaught error: ${err}`);
+    process.exit(1);
 });
