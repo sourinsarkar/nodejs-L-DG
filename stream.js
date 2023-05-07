891079b047ba76b7afc5b3ac3e3661ab803f7391
Sourin <iamsourinsarkar@gmail.com>
2023-05-07 17:18:38 +0530

    Implemented mkdir and rmdir and best practices of reading and writing files

--- stream.js
+++ stream.js
@@ -4,7 +4,27 @@ const path = require('path');
 const rs = fs.createReadStream(path.join(__dirname, 'files', 'lorem.txt'), 'utf8');
 const ws = fs.createWriteStream(path.join(__dirname, 'files', 'newLorem.txt'));
 
-rs.on('data', (chunk) => {
-    // console.log(chunk);
-    ws.write(chunk);
-});
+// rs.on('data', (chunk) => {
+    // console.log(chunk); // to print the file's content in console
+    // ws.write(chunk); // to write the file's content in some different file
+// });
+
+rs.pipe(ws);
+
+// --------------------------------------------------------------------------------------------------------
+
+// If a DIRECTORY doesn't exist -> create it
+if(!fs.existsSync('./new')) {
+    fs.mkdir('./new', (err) => {
+        if(err) throw err;
+        console.log('Directory created.');
+    });
+}
+
+// If a DIRECTORY exist -> delete it
+if(fs.existsSync('./new')) {
+    fs.rmdir('./new', (err) => {
+        if(err) throw err;
+        console.log('Directory Deleted.')
+    })
+}
