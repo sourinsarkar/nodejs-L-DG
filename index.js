0df5a71d7b64a9fed77a0bd058ff5c5a6d58dab4
Sourin <iamsourinsarkar@gmail.com>
2023-05-10 09:27:04 +0530

    Trial codes

---
+++ index.js
@@ -0,0 +1,8 @@
+// console.log('My Terminal');
+// console.log('My name is Sourin');
+
+const os = require('os');
+
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.type());
